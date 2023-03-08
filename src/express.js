e7ad2b3e3c6383334ac2b3cee0ad92eaf2c8773e
Jack-chau <chaukaho149@gmail.com>
2023-03-08 21:58:44 +0800

    npm install Postman

--- src/express.js
+++ src/express.js
@@ -1,13 +1,13 @@
 const express = require ( 'express' ) ;
 const app = express ( ) ;
-const PORT = 3000 ;
+const PORT = 2000 ;
 
 // End point ( PATH, function ) req = request, res = response
 app.get ( '/', ( req, res ) => { 
     res.send ( "Retrieve Data" ) ;
 } ) ;
 
-app.post ( '/', ( ) => {
+app.post ( '/', ( req, res ) => {
     res.send ( 'This is a post request' ) ;
 } ) ;
 
